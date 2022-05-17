4f410c22c023bd127bba3bd22741da2e09ca9e53
Anton Komrachkov <a.komrachkov@gmail.com>
2022-05-17 11:45:50 +0300

    Add try-with-resurces

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        
         int port = 8081;
         while (true) {
             try (ServerSocket serverSocket = new ServerSocket(port);
