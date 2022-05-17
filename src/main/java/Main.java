2472aaae214f562641f84f77b9f36c1b6558532c
Anton Komrachkov <a.komrachkov@gmail.com>
2022-05-17 11:38:02 +0300

    Port changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,21 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int port = 8080;
-        ServerSocket serverSocket = new ServerSocket(port);
+        int port = 8081;
         while (true) {
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-            System.out.println("New connection accepted");
+                System.out.println("New connection accepted");
 
-            final String name = in.readLine();
+                final String name = in.readLine();
 
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
